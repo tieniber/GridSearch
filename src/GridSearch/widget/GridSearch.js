c1ddc6c60816129a0f0eb6a7d50126afb7bfab29
mchmx <matthew.cha@mendix.com>
2018-05-10 14:20:24 -0400

    Split on Regex
    When splitting (OR condition) is enabled, the split string can now be interpreted as RegEx if desired.

--- src/GridSearch/widget/GridSearch.js
+++ src/GridSearch/widget/GridSearch.js
@@ -1,175 +1,269 @@
-define([
-    "dojo/_base/declare",
-	"GridSearch/widget/Core",
-    "dojo/dom-class",
-    "dojo/_base/lang",
-    "dojo/query",
+define( [
+  "dojo/_base/declare",
+  "GridSearch/widget/Core",
+  "dojo/dom-class",
+  "dojo/dom-style",
+  "dojo/_base/lang",
+  "dojo/query",
+  "dojo/text!GridSearch/widget/template/GridSearch.html"
+], function( declare, Core, dojoClass, dojoStyle, dojoLang, dojoQuery, widgetTemplate )
+{
+   "use strict";
 
-    "dojo/text!GridSearch/widget/template/GridSearch.html"
-], function(declare, Core, dojoClass, dojoLang, dojoQuery, widgetTemplate) {
-    "use strict";
+   return declare( "GridSearch.widget.GridSearch", [ Core ],
+   {
 
-    return declare("GridSearch.widget.GridSearch", [Core], {
+      templateString: widgetTemplate,
 
-        templateString: widgetTemplate,
+      // Internal variables.
+      _handles: null,
+      _searchNode: null,
 
-        // Internal variables.
-        _handles: null,
-        _contextObj: null,
-        //_searchMethod: "starts-with",
+      //modeler
+      gridEntity: null,
+      minCharacters: 0,
+      ta_rows: 0,
 
-        //modeler
-        gridEntity: null,
-		minCharacters: 0,
+      constructor: function()
+      {
+         this._handles = [];
+      },
 
-        constructor: function() {
-            this._handles = [];
-        },
+      postCreate: function()
+      {
+         logger.debug( this.id + ".postCreate" );
 
-        postCreate: function() {
-            logger.debug(this.id + ".postCreate");
+         this.connect( this.buttonNode, "click", "_clearAllSearchBoxes" );
+         this.connect( this._searchNode, "keyup", "_clearOnEscape" );
 
-            this.connect(this.buttonNode, "click", "_clearAllSearchBoxes");
-			this.connect(this.searchNode, "keyup", "_clearOnEscape");
+         //retrieve state (if available)
+         this.searchNodeSingle.value = this.getState( "searchValue", "" );
+         this._updateClearButtonRendering( this.searchNodeSingle );
+         this.searchNodeMulti.value = this.getState( "searchValue", "" );
+         this._updateClearButtonRendering( this.searchNodeMulti );
+      },
 
-			//retrieve state (if available)
-			this.searchNode.value = this.getState("searchValue", "");
-			this._updateClearButtonRendering();
-        },
-
-        update: function(obj, callback) {
-            logger.debug(this.id + ".update");
-			this._setupGrid(this._finishGridSetup.bind(this));
-            this._contextObj = obj;
-            this._updateRendering(callback);
-        },
-
-        resize: function(box) {
-            logger.debug(this.id + ".resize");
-        },
-
-        uninitialize: function() {
-            logger.debug(this.id + ".uninitialize");
-        },
-		storeState: function(t) {
-			t("searchValue", this.searchNode.value);
-		},
-        _updateRendering: function(callback) {
-            logger.debug(this.id + "._updateRendering");
-
-           if(callback) {callback()};
-		},
-		_finishGridSetup: function() {
-			if (this._grid) {
-                this.connect(this.searchNode, "keyup", "_searchKeyDown");
-				if (!this._grid.gridSearchWidgets) {
-					this._grid.gridSearchWidgets = {};
-				}
-				this._grid.gridSearchWidgets[this.id] = this;
-				//if the grid is set to wait for search, ensure we set the "_searchFilled" flag
-				if(this._grid.config && this._grid.config.gridpresentation && this._grid.config.gridpresentation.waitforsearch && this.searchNode.value) {
-					this._grid._searchFilled = true;
-				}
+      startup: function()
+      {
+         if ( this.renderAsTextarea )
+         {
+            console.log( "rendering textareaaa" );
+            this._searchNode = this.searchNodeMulti;
+            dojoStyle.set( this.searchNodeMulti, "display", "" );
+            if ( this.ta_rows > 0 )
+            {
+               dojoStyle.set( this.searchNodeMulti, "rows", this.ta_rows );
             }
-		},
-        _getSearchConstraint: function() {
-			var value = this.searchNode.value.replace(/''/g, '\'\''),
-				searchValues = [],
-                searchParams = [],
-                attributes = this.searchAttributes,
-				outvalue = "";
-            if (value && value.length >= this.minCharacters) {
-				if(!this.expertQuery) {
-					//basic or advanced search
-					if(this.allowOrConditions) {
-						searchValues = value.split(this.splitString);
-					} else {
-						searchValues.push(value);
-					}
+            dojoStyle.set( this.searchNodeSingle, "display", "none" );
+         }
+         else
+         {
+            console.log( "rendering textbox" );
+            this._searchNode = this.searchNodeSingle;
+         }
+      },
 
-					for (var i = 0, attr; attr = attributes[i]; ++i) {
-						if (attr.searchMethodParam === "startswith") {
-							attr._searchMethod = "starts-with";
-						} else {
-							attr._searchMethod = "contains";
-						}
+      update: function( obj, callback )
+      {
+         logger.debug( this.id + ".update" );
+         this._setupGrid( this._finishGridSetup.bind( this ) );
+         this._contextObj = obj;
+         this._updateRendering( callback );
+      },
 
-						if(!attr.customSearchEntity) {
-						//basic search
-							var searchPath = attr.searchAttributeParam.substring(0, attr.searchAttributeParam.lastIndexOf("/"));
-							var searchAttribute = attr.searchAttributeParam.substring(attr.searchAttributeParam.lastIndexOf("/") + 1, attr.searchAttributeParam.length);
+      resize: function( box )
+      {
+         logger.debug( this.id + ".resize" );
+      },
 
-							var curQuery = [];
-							if(searchPath) {
-								curQuery.push(searchPath + "[");
-							}
-							
-							for(var j=0; j < searchValues.length; j++) {
-								if(j > 0) {
-									curQuery.push(" or ");
-								}
-								var curValue = searchValues[j];
-								curQuery.push( attr._searchMethod + "(" + searchAttribute + ",'" + curValue + "')");
-							}
+      uninitialize: function()
+      {
+         logger.debug( this.id + ".uninitialize" );
+      },
+      storeState: function( t )
+      {
+         t( "searchValue", this._searchNode.value );
+      },
+      _updateRendering: function( callback )
+      {
+         logger.debug( this.id + "._updateRendering" );
 
-							if(searchPath) {
-								curQuery.push("]");
-							}
-							searchParams.push(curQuery.join(" "));
-						} else {
-						//advanced search
-							searchParams.push(attr.customSearchPath + "[" + attr._searchMethod + "(" + attr.customSearchAttribute + ",'" + curValue + "')]");
-						}
-					}
-					outvalue =  "[" + searchParams.join(" or ") + "]";
-				} else {
-					//expert search
-					outvalue = this.expertQuery.replace(/{\[1\]}/g, value).replace(/\r\n/g, ' ').replace(/''/g, '\'\'');
-				}
-			} else {
-            	outvalue = "";
-        	}
-
-			if (outvalue) {
-				this._currentFilter = value;
-			} else {
-				this._currentFilter = null;
-			}
-			//this.onSearchChanged();
-			return outvalue;
-        },
-        _updateClearButtonRendering: function() {
-        	if (this.searchNode.value === "") {
-                dojoClass.add(this.buttonNode, "hidden");
-            } else {
-                dojoClass.remove(this.buttonNode, "hidden");
+         if ( callback )
+         {
+            callback()
+         };
+      },
+      _finishGridSetup: function()
+      {
+         if ( this._grid )
+         {
+            this.connect( this._searchNode, "keyup", "_searchKeyDown" );
+            if ( !this._grid.gridSearchWidgets )
+            {
+               this._grid.gridSearchWidgets = {};
             }
-        },
-        _searchKeyDown: function() {
-			this._updateClearButtonRendering();
-			this._fireSearch();
-        },
-		_clearOnEscape: function(e) {
-		 if (e.keyCode == 27) {
-			 this._clearAllSearchBoxes();
-		 }
-		},
-        _clear: function() {
-            this.searchNode.value = "";
-            dojoClass.add(this.buttonNode, "hidden");
-			this._currentFilter = null;
-			this._fireSearch();
-		},
-        _reloadGrid: function() {
-			if (this._grid.update) {
-        		this._grid.update();
-        	} else if (this._grid.reload) {
-				this._grid.reload();
-        	} else {
-        		console.log("Could not find the grid refresh/reload function");
-        	}
-        }
-    });
-});
+            this._grid.gridSearchWidgets[ this.id ] = this;
+            //if the grid is set to wait for search, ensure we set the "_searchFilled" flag
+            if ( this._grid.config && this._grid.config.gridpresentation && this._grid.config.gridpresentation.waitforsearch && this._searchNode.value )
+            {
+               this._grid._searchFilled = true;
+            }
+         }
+      },
+      _getSearchConstraint: function()
+      {
+         var value = this._searchNode.value.replace( /''/g, '\'\'' ),
+            searchValues = [],
+            searchParams = [],
+            attributes = this.searchAttributes,
+            outvalue = "";
+         if ( value && value.length >= this.minCharacters )
+         {
+            if ( !this.expertQuery )
+            {
+               //basic or advanced search
+               if ( this.allowOrConditions )
+               {
+                  if ( this.useRegEx )
+                  {
+                     searchValues = value.trim().split( new RegExp( this.splitString ) );
+                  }
+                  else
+                  {
+                     searchValues = value.trim().split( this.splitString );
+                  }
+                  console.log( "split string: " + this.splitString );
+                  console.log( searchValues );
+               }
+               else
+               {
+                  searchValues.push( value );
+               }
 
-require(["GridSearch/widget/GridSearch"]);
+               for ( var i = 0, attr; attr = attributes[ i ]; ++i )
+               {
+                  if ( attr.searchMethodParam === "startswith" )
+                  {
+                     attr._searchMethod = "starts-with";
+                  }
+                  else
+                  {
+                     attr._searchMethod = "contains";
+                  }
+
+                  if ( !attr.customSearchEntity )
+                  {
+                     //basic search
+                     var searchPath = attr.searchAttributeParam.substring( 0, attr.searchAttributeParam.lastIndexOf( "/" ) );
+                     var searchAttribute = attr.searchAttributeParam.substring( attr.searchAttributeParam.lastIndexOf( "/" ) + 1, attr.searchAttributeParam.length );
+
+                     var curQuery = [];
+                     if ( searchPath )
+                     {
+                        curQuery.push( searchPath + "[" );
+                     }
+
+                     for ( var j = 0; j < searchValues.length; j++ )
+                     {
+                        if ( j > 0 )
+                        {
+                           curQuery.push( " or " );
+                        }
+                        var curValue = searchValues[ j ];
+                        curQuery.push( attr._searchMethod + "(" + searchAttribute + ",'" + curValue + "')" );
+                     }
+
+                     if ( searchPath )
+                     {
+                        curQuery.push( "]" );
+                     }
+                     searchParams.push( curQuery.join( " " ) );
+                  }
+                  else
+                  {
+                     //advanced search
+                     searchParams.push( attr.customSearchPath + "[" + attr._searchMethod + "(" + attr.customSearchAttribute + ",'" + curValue + "')]" );
+                  }
+               }
+               outvalue = "[" + searchParams.join( " or " ) + "]";
+            }
+            else
+            {
+               //expert search
+               outvalue = this.expertQuery.replace( /{\[1\]}/g, value ).replace( /\r\n/g, ' ' ).replace( /''/g, '\'\'' );
+            }
+         }
+         else
+         {
+            outvalue = "";
+         }
+
+         if ( outvalue )
+         {
+            this._currentFilter = value;
+         }
+         else
+         {
+            this._currentFilter = null;
+         }
+         //this.onSearchChanged();
+         return outvalue;
+      },
+      _updateClearButtonRendering: function( node )
+      {
+         var f;
+         if ( node )
+         {
+            f = ( node.value === "" )
+         }
+         else
+         {
+            f = ( this._searchNode.value === "" )
+         }
+         if ( f )
+         {
+            dojoClass.add( this.buttonNode, "hidden" );
+         }
+         else
+         {
+            dojoClass.remove( this.buttonNode, "hidden" );
+         }
+      },
+      _searchKeyDown: function()
+      {
+         this._updateClearButtonRendering();
+         this._fireSearch();
+      },
+      _clearOnEscape: function( e )
+      {
+         if ( e.keyCode == 27 )
+         {
+            this._clearAllSearchBoxes();
+         }
+      },
+      _clear: function()
+      {
+         this._searchNode.value = "";
+         dojoClass.add( this.buttonNode, "hidden" );
+         this._currentFilter = null;
+         this._fireSearch();
+      },
+      _reloadGrid: function()
+      {
+         if ( this._grid.update )
+         {
+            this._grid.update();
+         }
+         else if ( this._grid.reload )
+         {
+            this._grid.reload();
+         }
+         else
+         {
+            console.log( "Could not find the grid refresh/reload function" );
+         }
+      }
+   } );
+} );
+
+require( [ "GridSearch/widget/GridSearch" ] );
