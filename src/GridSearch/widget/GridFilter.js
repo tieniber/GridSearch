909cd3e92231ecf57b3e2c7767095dd7f38df039
Conner Charlebois <conner@C02WN0S7HV2V.local>
2018-07-26 10:32:23 -0400

    add option for default filter to static dropdown

--- src/GridSearch/widget/GridFilter.js
+++ src/GridSearch/widget/GridFilter.js
@@ -1,91 +1,101 @@
 define([
-    "dojo/_base/declare",
+	"dojo/_base/declare",
 	"GridSearch/widget/Core",
-    "dojo/_base/lang",
-    "dojo/query",
+	"dojo/_base/lang",
+	"dojo/query",
 	"dojo/dom-construct",
 
-    "dojo/text!GridSearch/widget/template/GridFilter.html"
-], function(declare, Core, dojoLang, dojoQuery, domConstruct, widgetTemplate) {
-    "use strict";
+	"dojo/text!GridSearch/widget/template/GridFilter.html"
+], function (declare, Core, dojoLang, dojoQuery, domConstruct, widgetTemplate) {
+	"use strict";
 
-    return declare("GridSearch.widget.GridFilter", [Core], {
+	return declare("GridSearch.widget.GridFilter", [Core], {
 
-        templateString: widgetTemplate,
+		templateString: widgetTemplate,
 
-        // Internal variables.
-        _handles: null,
-        _contextObj: null,
+		// Internal variables.
+		_handles: null,
+		_contextObj: null,
 
-        //modeler
+		//modeler
 		searchOptions: null,
 		blankOptionLabel: "",
 
 
-        postCreate: function() {
+		postCreate: function () {
 			logger.debug(this.id + ".postCreate");
 			this.superPostCreate();
 
 			this.blankOption.text = this.blankOptionLabel;
 
-			for (var i=0; i<this.searchOptions.length; i++) {
-				var optionLabel,tempOptionNode;
+			for (var i = 0; i < this.searchOptions.length; i++) {
+				var optionLabel, tempOptionNode;
 				optionLabel = this.searchOptions[i].optionLabel;
-				tempOptionNode = domConstruct.toDom("<option value=" + i + ">" + optionLabel + "</option>");
+				tempOptionNode = domConstruct.toDom("<option data-default='" + this.searchOptions[i].isDefault + "' value=" + i + ">" + optionLabel + "</option>");
 				domConstruct.place(tempOptionNode, this.selectNode);
 			}
 
 			//get dynamic options
-			if(this.useDynamicOptions) {
-				debugger;
+			if (this.useDynamicOptions) {
 				this._populateDynamicOptions();
 			}
 
 			//retrieve state (if available)
-			this.selectNode.value = this.getState("selection", "");
-        },
+			if (this.getState("selection", "")) {
+				this.selectNode.value = this.getState("selection", "");
+			} else {
+				// set to the default option
+				var defaultValue = this.selectNode.querySelector("[data-default]").value;
+				this.selectNode.value = defaultValue;
+			}
 
-        update: function(obj, callback) {
-            logger.debug(this.id + ".update");
+		},
+
+		update: function (obj, callback) {
+			logger.debug(this.id + ".update");
 
 			this._setupGrid(this._finishGridSetup.bind(this));
 
-            this._contextObj = obj;
-            if(callback) {callback()};
-        },
+			this._contextObj = obj;
+			if (callback) { callback() };
+		},
 
-        resize: function(box) {
-            logger.debug(this.id + ".resize");
-        },
-        uninitialize: function() {
-            logger.debug(this.id + ".uninitialize");
-        },
-		storeState: function(t) {
+		resize: function (box) {
+			logger.debug(this.id + ".resize");
+		},
+		uninitialize: function () {
+			logger.debug(this.id + ".uninitialize");
+		},
+		storeState: function (t) {
 			t("selection", this.selectNode.value);
 		},
-		_finishGridSetup: function() {
+		_finishGridSetup: function () {
 			if (this._grids) {
-                this.connect(this.selectNode, "onchange", "_fireSearch");
+				this.connect(this.selectNode, "onchange", "_fireSearch");
 				//if the grid is set to wait for search, ensure we set the "_searchFilled" flag
-				for (var i=0; i<this._grids.length; i++) {
+				for (var i = 0; i < this._grids.length; i++) {
 					var curGrid = this._grids[i];
-					if(curGrid.config && curGrid.config.gridpresentation && curGrid.config.gridpresentation.waitforsearch && this.searchNode.value) {
+					if (curGrid.config && curGrid.config.gridpresentation && curGrid.config.gridpresentation.waitforsearch && this.searchNode.value) {
 						curGrid._searchFilled = true;
 					}
-				}				
-            }
+				}
+				if (this.searchOptions.find(function (opt) { return opt.isDefault })) {
+					// there's a default search, so fire it.
+					this._fireSearch();
+				}
+			}
 		},
-		_populateDynamicOptions: function() {
+		_populateDynamicOptions: function () {
 			var args = {
 				xpath: this.dynamicOptionEntity,// + '[' + this.dynamicOptionXPath + ']',
 				callback: this._addDynamicOption
 			};
 			mx.data.get(args, this);
 		},
-		_addDynamicOption: function(obj) {
-			if(obj) {
+		_addDynamicOption: function (obj) {
+			if (obj) {
 				var nextIndex = this.searchOptions.length,
-				optionLabel,tempOptionNode;
+					optionLabel, tempOptionNode;
 				optionLabel = obj.getCaption(this.dynamicOptionAttribute);
 				optionValue = obj.get(this.dynamicOptionAttribute);
 				tempOptionNode = domConstruct.toDom("<option value=" + nextIndex + ">" + optionLabel + "</option>");
@@ -116,11 +126,11 @@ define([
 
 			this._reloadGrid();
 		},*/
-		_getSearchConstraint: function() {
+		_getSearchConstraint: function () {
 			var constraint,
-			outvalue = "";
+				outvalue = "";
 			if (this.selectNode.value === "") {
-				outvalue =  "";
+				outvalue = "";
 			} else {
 				outvalue = this.searchOptions[this.selectNode.value].optionXPath.replace(/\r\n/g, ' ');
 			}
@@ -133,11 +143,11 @@ define([
 			//this.onSearchChanged();
 			return outvalue;
 		},
-        _clear: function() {
-            this.selectNode.value = "";
+		_clear: function () {
+			this.selectNode.value = "";
 			this._currentFilter = null;
-        },
-    });
+		},
+	});
 });
 
 require(["GridSearch/widget/GridFilter"]);
