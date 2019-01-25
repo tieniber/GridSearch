2959f627a9c3c84d4ad6fae287bef5c24aa94611
Conner Charlebois <cdcharlebois@gmail.com>
2019-01-25 10:16:11 -0500

    fix logic with anysearch clear nanoflow

--- src/GridSearch/widget/AnySearch.js
+++ src/GridSearch/widget/AnySearch.js
@@ -41,7 +41,7 @@ define([
             //this function should clear the search widget
             this._contextObj.set(this.xpathAttribute, null);
             this._currentFilter = null;
-            if (this.onClearNf) {
+            if (!this._isEmptyObject(this.onClearNf)) {
                 // execute nanoflow to clear
                 mx.data.callNanoflow({
                     nanoflow: this.onClearNf,
@@ -125,6 +125,14 @@ define([
             });
             this._fireSearch();
         },
+
+        _isEmptyObject: function (obj) {
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key))
+                    return false;
+            }
+            return true;
+        }
     });
 });
 require(["GridSearch/widget/AnySearch"]);
